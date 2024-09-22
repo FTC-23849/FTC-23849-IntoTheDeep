9d52dddfd1eb720f4566c4e076aef1e03039c17b
ishaan11311 <141689894+ishaan11311@users.noreply.github.com>
2024-09-21 17:25:01 -0700

    Added new drive code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
@@ -0,0 +1,266 @@
+/*
+Copyright 2023 FIRST Tech Challenge Team FTC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+
+/**
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * Remove a @Disabled the on the next line or two (if present) to add this opmode to the Driver Station OpMode list,
+ * or add a @Disabled annotation to prevent this OpMode from being added to the Driver Station
+ */
+@TeleOp(name = "DriverControl", group = "stuff")
+
+public class DriverControl extends OpMode {
+    /* Declare OpMode members. */
+    DcMotorEx leftFrontMotor;
+    DcMotorEx rightFrontMotor;
+    DcMotorEx leftBackMotor;
+    DcMotorEx rightBackMotor;
+    DcMotor intake;
+    DcMotor liftMotor;
+
+    CRServo tapeMeasureServo;
+    Servo planeLauncherServo;
+    CRServo outtakeWheelServo;
+    DcMotorEx slideMotor1;
+    DcMotorEx slideMotor2;
+    Servo elbowServo;
+    Servo wristServo;
+    Servo outtakeDoorServo;
+
+
+
+    ElapsedTime mStateTime = new ElapsedTime();
+    boolean outtakeArmUp = false;
+
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+
+
+
+        leftFrontMotor  = hardwareMap.get(DcMotorEx.class,"LF");
+        rightFrontMotor = hardwareMap.get(DcMotorEx.class, "RF");
+        leftBackMotor = hardwareMap.get(DcMotorEx.class,"LB");
+        rightBackMotor = hardwareMap.get(DcMotorEx.class,"RB");
+        leftFrontMotor.setDirection(DcMotorEx.Direction.REVERSE);
+        leftBackMotor.setDirection(DcMotorEx.Direction.REVERSE);
+
+        leftFrontMotor.setZeroPowerBehavior(BRAKE);
+        leftBackMotor.setZeroPowerBehavior(BRAKE);
+        rightFrontMotor.setZeroPowerBehavior(BRAKE);
+        rightBackMotor.setZeroPowerBehavior(BRAKE);
+    }
+
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        double xDistance = 0;
+        double yDistance = 0;
+        double speed = 0;
+        double direction = 0;
+        int preciseSpeedDivider = 3;
+        boolean preciseDriving;
+//        telemetry.addData("voltage-leftFrontMotor", leftFrontMotor.getCurrent(CurrentUnit.AMPS));
+//        telemetry.addData("voltage-leftBackMotor", leftBackMotor.getCurrent(CurrentUnit.AMPS));
+//        telemetry.addData("voltage-rightFrontMotor", rightFrontMotor.getCurrent(CurrentUnit.AMPS));
+//        telemetry.addData("voltage-rightBackMotor", rightBackMotor.getCurrent(CurrentUnit.AMPS));
+//        telemetry.update();
+
+        //drivetrain
+
+
+
+        // 57.29577951 = 1 radian = 180/pi
+        if ((Math.atan2(gamepad1.left_stick_x, -gamepad1.left_stick_y) * 57.29577951) < 0){
+            direction = (Math.atan2(gamepad1.left_stick_x, -gamepad1.left_stick_y)) * 57.29577951;
+            direction += 360;
+        }
+        else {
+            direction = (Math.atan2(gamepad1.left_stick_x, -gamepad1.left_stick_y)) * 57.29577951;
+        }
+
+        double joystickDriftTolerance = 0.05;
+        if(Math.abs(speed) > joystickDriftTolerance) {
+            drive(direction, speed, gamepad1.right_stick_x * speed, -gamepad1.right_stick_x * speed);
+        }
+        else {
+            double turningSpeed = gamepad1.right_stick_x / 1.2;
+            turn(turningSpeed);
+        }
+
+
+
+
+
+        //Set the power of the slide motors to the value of the right stick on gamepad2
+
+
+
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+        //myRobot.OuttakeArmRest(elbowServo, wristServo, outtakeDoorServo, outtakeWheelServo);
+    }
+    public void drive (double direction, double speed, double leftOffset, double rightOffset){
+//        leftFrontMotor  = hardwareMap.get(DcMotorEx.class,"LF");
+//        rightFrontMotor = hardwareMap.get(DcMotorEx.class, "RF");
+//        leftBackMotor = hardwareMap.get(DcMotorEx.class,"LB");
+//        rightBackMotor = hardwareMap.get(DcMotorEx.class,"RB");
+//        leftFrontMotor.setDirection(DcMotorEx.Direction.REVERSE);
+//        leftBackMotor.setDirection(DcMotorEx.Direction.REVERSE);
+//
+//        leftFrontMotor.setZeroPowerBehavior(BRAKE);
+//        leftBackMotor.setZeroPowerBehavior(BRAKE);
+//        rightFrontMotor.setZeroPowerBehavior(BRAKE);
+//        rightBackMotor.setZeroPowerBehavior(BRAKE);
+
+        double difference = 0;
+        double lf = 0;
+        double rf = 0;
+        double lb = 0;
+        double rb = 0;
+
+        if(direction >= 0 && direction < 45){
+            difference = 10 - direction / 4.5;
+            rf = difference / 10 * speed;
+            lb = difference / 10 * speed;
+            rb = speed;
+            lf = speed;
+        }
+        else if(direction >= 45 && direction < 90){
+            difference = (direction - 45) * (1 / -4.5);
+            rf = difference / 10 * speed;
+            lb = difference / 10 * speed;
+            rb = speed;
+            lf = speed;
+        }
+        else if(direction >= 90 && direction < 135){
+            difference = 10 - (direction - 90) / 4.5;
+            rf = -speed;
+            lb = -speed;
+            rb = difference / 10 * speed;
+            lf = difference / 10 * speed;
+        }
+        else if(direction >= 135 && direction < 180){
+            difference = (direction - 135) * (1 / -4.5);
+            rf = -speed;
+            lb = -speed;
+            rb = difference / 10 * speed;
+            lf = difference / 10 * speed;
+        }
+        else if(direction >= 180 && direction < 225){
+            difference = -10 - (direction - 180) / -4.5;
+            rf = difference / 10 * speed;
+            lb = difference / 10 * speed;
+            rb = -speed;
+            lf = -speed;
+        }
+        else if (direction >= 225 && direction < 270){
+            difference = (direction-225) * (1 / 4.5);
+            rf = difference / 10 * speed;
+            lb = difference / 10 * speed;
+            rb = -speed;
+            lf = -speed;
+        }
+        else if (direction >= 270 && direction < 315){
+            difference = -10 - (direction - 270) / -4.5;
+            rf = speed;
+            lb = speed;
+            rb = difference / 10 * speed;
+            lf = difference / 10 * speed;
+        }
+        else {
+            difference = (direction - 315) * (1 / 4.5);
+            rf = speed;
+            lb = speed;
+            rb = difference / 10 * speed;
+            lf = difference / 10 * speed;
+        }
+
+        rf = rf + rightOffset;
+        rb = rb + rightOffset;
+        lf = lf + leftOffset;
+        lb = lb + leftOffset;
+
+
+
+
+            leftFrontMotor.setPower(lf);
+            leftBackMotor.setPower(lb);
+            rightFrontMotor.setPower(rf);
+            rightBackMotor.setPower(rb);
+
+
+    }
+    public void turn ( double speed){
+
+        leftFrontMotor  = hardwareMap.get(DcMotorEx.class,"LF");
+        rightFrontMotor = hardwareMap.get(DcMotorEx.class, "RF");
+        leftBackMotor = hardwareMap.get(DcMotorEx.class,"LB");
+        rightBackMotor = hardwareMap.get(DcMotorEx.class,"RB");
+        leftFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftBackMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftFrontMotor.setPower(speed);
+        leftBackMotor.setPower(speed);
+        rightFrontMotor.setPower(speed*-1);
+        rightBackMotor.setPower(speed*-1);
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlgm0.java
@@ -0,0 +1,97 @@
+/*
+Copyright 2023 FIRST Tech Challenge Team FTC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+
+/**
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * Remove a @Disabled the on the next line or two (if present) to add this opmode to the Driver Station OpMode list,
+ * or add a @Disabled annotation to prevent this OpMode from being added to the Driver Station
+ */
+@TeleOp(name = "DriverControl", group = "stuff")
+
+public class DriverControlgm0 extends OpMode {
+    /* Declare OpMode members. */
+    DcMotorEx leftFrontMotor;
+    DcMotorEx rightFrontMotor;
+    DcMotorEx leftBackMotor;
+    DcMotorEx rightBackMotor;
+    DcMotor intake;
+    DcMotor liftMotor;
+
+    CRServo tapeMeasureServo;
+    Servo planeLauncherServo;
+    CRServo outtakeWheelServo;
+    DcMotorEx slideMotor1;
+    DcMotorEx slideMotor2;
+    Servo elbowServo;
+    Servo wristServo;
+    Servo outtakeDoorServo;
+
+
+    ElapsedTime mStateTime = new ElapsedTime();
+    boolean outtakeArmUp = false;
+
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+
+        leftFrontMotor = hardwareMap.get(DcMotorEx.class, "LF");
+        rightFrontMotor = hardwareMap.get(DcMotorEx.class, "RF");
+        leftBackMotor = hardwareMap.get(DcMotorEx.class, "LB");
+        rightBackMotor = hardwareMap.get(DcMotorEx.class, "RB");
+        leftFrontMotor.setDirection(DcMotorEx.Direction.REVERSE);
+        leftBackMotor.setDirection(DcMotorEx.Direction.REVERSE);
+
+        leftFrontMotor.setZeroPowerBehavior(BRAKE);
+        leftBackMotor.setZeroPowerBehavior(BRAKE);
+        rightFrontMotor.setZeroPowerBehavior(BRAKE);
+        rightBackMotor.setZeroPowerBehavior(BRAKE);
+    }
+    @Override
+    public void loop() {
+        double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
+
+        leftFrontMotor.setPower(y + x + rx);
+        leftBackMotor.setPower(y - x + rx);
+        rightFrontMotor.setPower(y - x - rx);
+        rightBackMotor.setPower(y + x - rx);
+    }
+}
