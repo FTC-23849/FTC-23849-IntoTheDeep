3b4100f89195b65eb5c578f82e5ab23abea4182e
ishaan11311 <141689894+ishaan11311@users.noreply.github.com>
2024-10-12 19:11:16 -0700

    vision kind of works. distance calculations needs to be tuned

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/BasicSampleTracker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/BasicSampleTracker.java
@@ -8,8 +8,12 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
+import org.opencv.core.RotatedRect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
@@ -19,6 +23,7 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Autonomous
@@ -30,7 +35,7 @@ public class BasicSampleTracker extends LinearOpMode {
 
     // Calculate the distance using the formula
     public static final double objectWidthInRealWorldUnits = 3.75;  // Replace with the actual width of the object in real-world units
-    public static final double focalLength = 728;
+    //public static final double focalLength = 728;
 
     @Override
     public void runOpMode(){
@@ -45,6 +50,7 @@ public class BasicSampleTracker extends LinearOpMode {
 
         }
         controlHubCam.stopStreaming();
+        controlHubCam.closeCameraDevice();
 
     }
 
@@ -54,7 +60,7 @@ public class BasicSampleTracker extends LinearOpMode {
 
         Mat outPut = new Mat();
 
-        Scalar rectColor = new Scalar(255.0, 0.0, 0.0);
+        //Scalar rectColor = new Scalar(255.0, 0.0, 0.0);
 
         @Override
         public Mat processFrame(Mat input) {
@@ -66,20 +72,49 @@ public class BasicSampleTracker extends LinearOpMode {
             Mat hierarchy = new Mat();
             Imgproc.findContours(yellowMask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
+            double focalLength = 379; // Approximate focal length in pixels for Logitech C920
+            double realWidth = 8.9; // Real width of the sample in cm
+            double realHeight = 3.8; //Real height of the sample in cm
 
             for (MatOfPoint contour : contours) {
-                // Get the bounding box of the contour
-                Rect boundingBox = Imgproc.boundingRect(contour);
 
-                // Draw the bounding box on the original image
-                Imgproc.rectangle(input, boundingBox.tl(), boundingBox.br(), new Scalar(0, 255, 0), 2);
+                // Filter out small contours based on area
+                if (Imgproc.contourArea(contour) > 1000) {  // Adjust this threshold based on real-world testing
+                    // Use minAreaRect to get the minimum enclosing rectangle
+                    RotatedRect rotatedRect = Imgproc.minAreaRect(new MatOfPoint2f(contour.toArray()));
 
+                    // Get the four corners of the rectangle
+                    Point[] rectPoints = new Point[4];
+                    rotatedRect.points(rectPoints);
+
+                    // Calculate distance to the object
+                    double heightInPixels = rotatedRect.size.height;// Height of the rectangle in pixels
+                    double widthInPixels = rotatedRect.size.width;// Width of the rectangle in pixels
+                    double distance = (focalLength * realWidth) / widthInPixels;
+                    double distanceBasedOnHeight = (focalLength * realHeight) / heightInPixels;
+                    double averageDistance = (distance + distanceBasedOnHeight) / 2;
+
+                    // Calculate center point for distance text
+                    Point center = new Point((rectPoints[0].x + rectPoints[2].x) / 2, (rectPoints[0].y + rectPoints[2].y) / 2);
+                    String distanceText = String.format("%.2f cm", averageDistance);
+                    Imgproc.putText(input, distanceText, center, Imgproc.FONT_HERSHEY_SIMPLEX, 0.5, new Scalar(255, 255, 255), 1);
+
+                    // Draw the parallelogram on the input image
+                    Imgproc.polylines(input, Arrays.asList(new MatOfPoint(rectPoints)), true, new Scalar(0, 255, 0), 2);
+                    telemetry.addData("Pixel Height", rotatedRect.size.height);
+                    telemetry.addData("Pixel Width", rotatedRect.size.width);
+                    telemetry.update();
+                }
             }
 
             input.copyTo(outPut);
 
+            yellowMask.release();
+            hierarchy.release();
+
+
             return outPut;
-            
+
         }
 
     }
@@ -97,6 +132,9 @@ public class BasicSampleTracker extends LinearOpMode {
         Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
         Imgproc.morphologyEx(redMask, redMask, Imgproc.MORPH_OPEN, kernel);
         Imgproc.morphologyEx(redMask, redMask, Imgproc.MORPH_CLOSE, kernel);
+
+        hsvFrame.release();
+        kernel.release();
 
         return redMask;
     }
